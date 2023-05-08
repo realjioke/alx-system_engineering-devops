a691c510ad5f845ef2b54f7f804864e6c6c2ae5f
realjioke <scarlobest@gmail.com>
2023-05-07 21:18:14 -0700

    100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -2,6 +2,11 @@ file { '/etc/ssh/ssh_config':
   ensure  => present,
   content => "    PasswordAuthentication no\n    IdentityFile ~/.ssh/school\n",
   mode    => '0644',
+}
+
+exec { 'echo':
   path    => '/usr/bin:/bin',
+  command => 'echo "    PasswordAuthentication no\n    IdentityFile ~/.ssh/school" >> /etc/ssh/ssh_config',
+  returns => [0,1]
 }
 
