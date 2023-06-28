cac37a3199f1585948a31695b1de64fde2e17e90
Your Name <scarlobest@gmail.com>
2023-06-28 07:46:25 -0700

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,5 +1,25 @@
 # Install nginx with puppet
-exec { 'install':
-  command => 'sudo apt-get -y update && sudo apt-get -y install nginx && echo "Hello World!" | sudo tee /var/www/html/index.nginx-debian.html && sudo sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/realjioke permanent;/" /etc/nginx/sites-available/default && sudo systemctl restart nginx',
+exec { 'install_nginx':
+  command  => 'apt-get -y update && apt-get -y install nginx',
+  path     => ['/usr/bin', '/usr/sbin', '/bin', '/sbin'],
+  provider => shell,
+}
+
+exec { 'create_index_file':
+  command  => 'echo "Hello World!" | tee /var/www/html/index.nginx-debian.html',
+  path     => ['/usr/bin', '/usr/sbin', '/bin', '/sbin'],
+  provider => shell,
+}
+
+exec { 'configure_redirect':
+  command  => 'sed -i "s/server_name _;/server_name _;\n\trewrite ^\/redirect_me https:\/\/github.com\/jesulayomy permanent;/" /etc/nginx/sites-available/default',
+  path     => ['/usr/bin', '/usr/sbin', '/bin', '/sbin'],
+  provider => shell,
+}
+
+exec { 'start_nginx':
+  command  => 'service nginx start',
+  path     => ['/usr/bin', '/usr/sbin', '/bin', '/sbin'],
+  provider => shell,
 }
 
